441b624723aea42e9e8546f14f2217046404d699
ShunLi <15363540600@163.com>
2022-11-12 09:53:08 +1000

    backend router and controller logic

--- server/server.js
+++ server/server.js
@@ -1,14 +1,23 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
 import express from "express";
 import path from 'path';
 import {fileURLToPath} from 'url';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
 import rootRoutes from './routes/root.js';
 import notFoundRoutes from './routes/notFound.js';
+import userRoutes from './routes/user.js';
+import noteRoutes from './routes/note.js';
+
 import {logger}  from './middleware/logger.js';
 import errorHandler  from './middleware/errorHandler.js';
 import corsOptions from './config/corsOptions.js';
+import conncetDB from './config/dbconn.js';
+import { logEvents } from './middleware/logger.js';
+
 
 
 const __filename = fileURLToPath(import.meta.url);
@@ -17,6 +26,7 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 3500;
 
+conncetDB();
 
 app.use(logger);
 
@@ -27,8 +37,19 @@ app.use(cookieParser());
 app.use('/', express.static(path.join(__dirname, 'public')));
 app.use('/', rootRoutes);
 
+app.use('/users', userRoutes);
+app.use('/notes', noteRoutes);
+
 app.all('*', notFoundRoutes);
 
 app.use(errorHandler);
 
-app.listen(PORT, () => console.log(`connected on ${PORT}`));
+mongoose.connection.once('open', () => {
+    console.log('connected to mongoDB');
+    app.listen(PORT, () => console.log(`connected on ${PORT}`));
+});
+
+mongoose.connection.on('error', err => {
+    console.log(err)
+    logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log')
+});
